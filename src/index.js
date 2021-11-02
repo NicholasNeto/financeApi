4e9b065021f6674571b7cf70c054e30334db10d8
NicholasNeto <nicholasneto@outlook.com>
2021-11-02 19:10:19 -0300

    add rule about customer not found

--- src/index.js
+++ src/index.js
@@ -38,6 +38,11 @@ app.get('/statement/:cpf', (request, response) => {
     const { cpf } = request.params
 
     const customer = customers.find((customer) => customer.cpf === cpf)
+
+    if(!customer){
+        return response.status(400).json({ error: 'Customer not found' })
+    }
+
     return response.status(200).json(customer.statement)
 })
 
