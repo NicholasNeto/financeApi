7763095e33f9d7736064836fdac45b6e54e19a1d
NicholasNeto <nicholasneto@outlook.com>
2021-11-02 18:41:20 -0300

    created account customers with id, name, cpf, statement

--- src/index.js
+++ src/index.js
@@ -1,5 +1,34 @@
 const express = require('express');
+const { v4: uuidV4 } = require('uuid');
+
 
 const app = express();
+
+app.use(express.json())
+
+const customers = []
+
+
+/*
+    name: string;
+    cpf: string,
+    id: uuidV4(),
+    statement: string[]
+*/
+
+app.post('/account', (request, response) => {
+    const { cpf, name } = request.body;
+
+    customers.push({
+        name,
+        cpf,
+        id: uuidV4(),
+        statement: []
+    })
+
+    return response.status(201).send()
+
+})
+
 
 app.listen(3333)
