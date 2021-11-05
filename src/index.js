7a41c9e603cefa7ee604db1afaf291b4cceb4fed
NicholasNeto <nicholasneto@outlook.com>
2021-11-04 21:06:32 -0300

    delete account

--- src/index.js
+++ src/index.js
@@ -76,6 +76,13 @@ app.get('/account', verifyIfExistsAccountCPF, (request, response) => {
     return response.status(200).json(customer)
 })
 
+app.delete('/account', verifyIfExistsAccountCPF, (request, response) => {
+    const { customer } = request
+
+    customers.splice(customer, 1)
+    return response.status(200).json(customers)
+})
+
 app.get('/statement', verifyIfExistsAccountCPF, (request, response) => {
     const { customer } = request
     return response.status(200).json(customer.statement)
